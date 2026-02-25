7bc5422655e7148f9be27f1c589846e1cad145be
MJ289-oss <moisescerqueira1010@gmail.com>
2026-02-25 20:19:29 -0300

    atualização

--- ddl/Script-1.sql
+++ ddl/Script-1.sql
@@ -68,38 +68,38 @@ create table consulta (
 --script terceira parte:
 
 create table procedimento (
-    id_procedimento int not null,
+    id int not null,
     nome varchar(50) not null,
-    descricao_tecnica varchar(100) not null,
-    constraint pk_id_procedimento primary key (id_procedimento)
+    descricao_tecnica text not null,
+    constraint pk_id_procedimento primary key (id)
 )
 
 create table historico_preco (
-    id_historico int not null,
-    valor decimal(5,2) not null,
+    id int not null,
+    valor decimal(6,2) not null,
     data_inicio date not null,
     data_fim date,
-    constraint pk_id_historico_preco primary key (id_historico)
+    constraint pk_id_historico_preco primary key (id)
 )
 
 create table convenio (
-    id_convenio int not null,
+    id int not null,
     nome_empresa varchar(20) not null,
     cnpj varchar(14) not null,
     registro_ans varchar(6) not null,
-    constraint pk_id_convenio primary key (id_convenio)
+    constraint pk_id_convenio primary key (id)
 )
 
 create table pagamento (
-    id_pagamento int not null,
-    valor_pago decimal(5,2) not null,
-    constraint pk_id_pagamento primary key (id_pagamento)
+    id int not null,
+    valor_pago decimal(6,2) not null,
+    constraint pk_id_pagamento primary key (id)
 )
 
 create table forma_pagamento (
-    id_forma_pagamento int not null,
+    id int not null,
     descricao varchar(20) not null,
-    constraint pk_id_forma_pagamento primary key (id_forma_pagamento)
+    constraint pk_id_forma_pagamento primary key (id)
 )
 
 -- quarta parte --
